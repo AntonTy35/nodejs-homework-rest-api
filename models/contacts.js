0cbe3268480f2c7a453383b8e3eadf00f96bd38e
AntonTy35 <antony.tislenko@gmail.com>
2023-11-11 13:45:01 +0200

    hw02-express

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,53 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require("path");
+const crypto =require("crypto");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "..", "models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const rawJSON = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(rawJSON);
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contactByID = contacts.find((contact) => contact.id === contactId);  
+  return contactByID || null;
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
 
-const updateContact = async (contactId, body) => {}
+  if (index === -1) {
+    return null;
+  }
+  const newContacts = [...contacts.slice(0, index), ...contacts.slice(index + 1)];
+
+  await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
+
+  return contacts[index];  
+};
+
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = {id: crypto.randomUUID(), ...body};
+
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id: contactId, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
 
 module.exports = {
   listContacts,
