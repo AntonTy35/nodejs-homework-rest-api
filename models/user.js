a376a90d13d47dcbbdad495427d06a624d8dd156
AntonTy35 <antony.tislenko@gmail.com>
2023-11-28 14:41:18 +0200

    hw-05

--- models/user.js
+++ models/user.js
@@ -27,6 +27,10 @@ const userSchema = new Schema(
       type: String,
       default: "",
     },
+    avatarURL: {
+      type: String,
+      default: null,
+    },
   },
   { versionKey: false, timestamps: true }
 );
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 
 const { schemas } = require("../../utils/validation/userValidationSchemas");
 
@@ -23,6 +23,13 @@ router.patch(
   authenticate,
   validateBody(schemas.subscriptionSchema),
   ctrl.updateSubscription
+);
+
+router.patch(
+  "/avatar",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.uploadAvatar
 );
 
 module.exports = router;
